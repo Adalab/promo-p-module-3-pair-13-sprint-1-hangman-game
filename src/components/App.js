fe2711367fd821288b73403d350250474c6ae86e
NaiSaratxaga <nsaratxaga@gmail.com>
2022-04-19 10:24:07 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import logo from '../images/logo.svg';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis alias fuga odio odit atque expedita assumenda numquam quidem, voluptatibus facere ullam eos quos iusto beatae sit perspiciatis dolore laudantium minus.
+        </p>
+    </div>
+  );
+}
+
+export default App;
